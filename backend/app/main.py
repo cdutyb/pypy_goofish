e41290b3fb715709786db01c741eb6f5ef1ec216
su322 <85404658+su322@users.noreply.github.com>
2025-01-25 23:41:13 +0800

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,8 +10,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5000", "http://localhost:5001",
-                   "http://127.0.0.1:5000", "http://127.0.0.1:5001"],  # 前端的地址，允许凭证时 allow_origins 不能设定为 ['*']，必须指定源
+    allow_origins=["http://localhost:5000", "http://127.0.0.1:5000",
+                   "http://localhost:5001", "http://127.0.0.1:5001",
+                   "http://localhost:5002", "http://127.0.0.1:5002",
+                   "http://localhost:5003", "http://127.0.0.1:5003"],  # 前端的地址，允许凭证时 allow_origins 不能设定为 ['*']，必须指定源
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
