88b410d4a2054730cc17969464684a7ce88b1aa5
su322 <85404658+su322@users.noreply.github.com>
2025-01-25 14:37:31 +0800

    Update workflow_test.py

--- backend/app/core/workflow_test.py
+++ backend/app/core/workflow_test.py
@@ -1,3 +1,9 @@
+import os
+
 import workflow
 
-workflow.workflow("intela770", 10, "../data/intela770_items.csv")
+# workflow.workflow("intela770", 10, "../data/intela770_items.csv")
+# print(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data"))
+# current_dir = os.path.dirname(os.path.abspath(__file__))
+# driver_path = os.path.join(current_dir, 'edgedriver_win64', 'msedgedriver.exe')
+# print(driver_path)
